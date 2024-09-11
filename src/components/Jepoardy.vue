f951e1af4304faafae790e1b60292f1a10135fc8
Kev Nangcas <knangcas@asu.edu>
2024-09-11 18:11:04 -0500

    first commit

---
+++ src/components/Jepoardy.vue
@@ -0,0 +1,160 @@
+<script>
+  export default {
+    name: 'Jeopardy',
+    props: {
+      name: String
+    },
+    data() {
+        return {
+            msg: "Hello ",
+            amt: 25,
+            questions: [],
+            cat1: [],
+            cat2: [],
+            cat3: [],
+            cat4: [],
+            avoidCats: [10,13,21,26,27,29,30,32]
+        }
+    },
+    methods: {
+        randomCat() {
+            let rval = Math.floor( Math.random() * (24) + 9);
+            while (this.avoidCats.includes(rval)) {
+                rval = Math.floor( Math.random() * (24) + 9);
+            }
+            return rval;
+        },
+        get4cats() {
+            let catArray = [];
+            let cat = this.randomCat();
+            while(catArray.length != 4) {
+                while (catArray.includes(cat)) {
+                    cat = this.randomCat();
+                }
+                catArray.push(cat);
+            }
+            return catArray;
+        },
+        setupBox() {
+            let catArray= this.get4cats();
+            console.log(catArray);
+            let col1 = this.getColumn(catArray[0]);
+            //let col2 = this.getColumn(catArray[1]);
+            //let col3 = this.getColumn(catArray[2]);
+            //let col4 = this.getColumn(catArray[3]);
+
+        },
+        getColumn(num) {
+            let urls = this.buildURL(num);
+            console.log(urls);
+
+            setTimeout(this.fetchCat(urls[0]), 10000);
+            setTimeout(this.fetchCat(urls[1]), 10000);
+            setTimeout(this.fetchCat(urls[2]), 10000);
+        
+
+        },
+
+        buildURL(num) {
+            let urlEasyPre="https://opentdb.com/api.php?amount=2&category="
+            let urlEasySuf="&difficulty=easy&type=boolean"
+            let urlMediumPre="https://opentdb.com/api.php?amount=2&category="
+            let urlMediumSuf="&difficulty=medium&type=boolean"
+            let urlHardPre="https://opentdb.com/api.php?amount=1&category="
+            let urlHardSuf="&difficulty=hard&type=boolean"
+
+            let easy = urlEasyPre + num + urlEasySuf;
+            let medium = urlMediumPre + num + urlMediumSuf;
+            let hard = urlHardPre + num + urlHardSuf;
+
+            return [easy, medium, hard]
+        },
+
+        fetchCat(url) {
+            
+            fetch(url).then(response => {
+                if (!response.ok) {
+                    throw new Error("Could not fetch category");
+                }
+                return response.json(); }
+            ).then(data => {
+                if (data.response_code===0) {
+                for (let i = 0; i < data.results.length; i++) {
+                    this.questions.push(data.results[i].question);
+                }
+
+                
+                } else {
+                    console.log(data.response_code);
+                    throw new Error("Error fetching question");
+                }
+        })
+            .catch(error => console.error(error));;
+
+        },
+
+        delayCall() {
+            setTimeout(()=> console.log("waited"), 5500);
+        }
+
+    },
+    mounted() {
+        this.setupBox();
+        console.log(this.questions);
+    }
+  }
+</script>
+
+<template>
+
+<div class="grid-container">
+    <div class="grid-item" @click="setupBox()">cat1</div>
+  <div class="grid-item">cat2</div>
+  <div class="grid-item">cat3</div>  
+  <div class="grid-item">cat4</div>
+  <div class="grid-item">$100</div>
+  <div class="grid-item">$100</div>
+  <div class="grid-item">$100</div>  
+  <div class="grid-item">$100</div>
+  <div class="grid-item">$200</div>
+  <div class="grid-item">$200</div>
+  <div class="grid-item">$200</div>
+  <div class="grid-item">$200</div>
+  <div class="grid-item">$300</div>
+  <div class="grid-item">$300</div>
+  <div class="grid-item">$300</div>
+  <div class="grid-item">$300</div>
+  <div class="grid-item">$400</div>
+  <div class="grid-item">$400</div>
+  <div class="grid-item">$400</div>
+  <div class="grid-item">$400</div>
+  <div class="grid-item">$500</div>
+  <div class="grid-item">$500</div>
+  <div class="grid-item">$500</div>
+  <div class="grid-item">$500</div>
+  
+</div>
+
+</template>
+
+<style scoped>
+
+.grid-container {
+    display: grid;
+    grid-template-columns: auto auto auto auto;
+    padding: 10px;
+    background-color: blue;
+
+}
+.grid-item {
+  background-color: rgba(25, 0, 138, 0.8);
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 20px;
+  font-size: 30px;
+  text-align: center;
+  color: orange; 
+}
+.grid-item:hover {
+    background-color: blue
+}
+</style>
---
+++ src/components/Title.vue
@@ -0,0 +1,45 @@
+<script>
+  export default {
+    name: 'Title',
+    props: {
+      name: String
+    },
+    data() {
+        return {
+            msg: "J  E  O  P  A  R  D  Y",
+            amt: 25
+        }
+    },
+  }
+</script>
+
+<template>
+  
+  <h1 class="titleTop">{{ msg }}</h1>
+ 
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+  text-align: center;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
